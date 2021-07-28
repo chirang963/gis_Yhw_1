861047f59c4b6c4765624a632ad1e83d98614f4e
qawsedrf963 <qawsedrf963@naver.com>
2021-07-28 12:11:33 +0900

    7/28 ban class commit 2

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
@@ -7,8 +7,11 @@ from profileapp.models import Profile
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+
     template_name = 'profileapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -18,5 +21,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})

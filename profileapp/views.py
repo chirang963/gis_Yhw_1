ad502e9bdba0919db45c7191e2c713fc0d2799a3
qawsedrf963 <qawsedrf963@naver.com>
2021-07-28 12:34:56 +0900

    7/28 ban class commit 3

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -17,11 +19,15 @@ class ProfileCreateView(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
     template_name = 'profileapp/update.html'
+
+
 
     def get_success_url(self):
         return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
